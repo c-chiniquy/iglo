1234d72359c302c363031ce93a1bca88ffc2e40a
c-chiniquy <66140054+c-chiniquy@users.noreply.github.com>
2025-02-27 14:51:58 +0100

    Improved the build system. No more nuget.exe.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-REM This batch file builds the iglo example projects for Visual Studio 2019.
+REM This batch file builds for Visual Studio 2019.
 cmake -B build -G "Visual Studio 16 2019" -A x64
 start build/iglo.sln
